b07f07c01a726c4242f73ccaa9743fd045100665
nualagr <nualagreenwood@gmail.com>
2020-11-25 16:30:33 +0000

    feat: Add testimonial carousel, welcome banner and registration buttons to home page. Style book cover url coming from database.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
   $(document).ready(function(){
-    /* Initialization of the side-nav trigger taken from https://materializecss.com/navbar.html*/
+    /* Initialization of the side-nav trigger taken from https://materializecss.com/navbar.html */
     $('.sidenav').sidenav();
+    /* Initialization of the carousel taken from https://materializecss.com/carousel.html */
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
   });
