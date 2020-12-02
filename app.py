f44048bfb5042e150fe23cd86753c8321622d660
nualagr <nualagreenwood@gmail.com>
2020-12-02 12:03:01 +0000

    feat: Displaying one linked review under the view_book.html information. Still feeding the book_id in manually.

--- app.py
+++ app.py
@@ -152,8 +152,12 @@ def view_book(book_id):
     this_book = mongo.db.books.find_one(
         {"_id": ObjectId(book_id)}
     )
-    print(type(this_book))
-    return render_template("view_book.html", this_book=this_book)
+    this_book_review = mongo.db.reviews.find_one(
+        {"book_id": ObjectId(book_id)}
+    )
+    return render_template(
+        "view_book.html", this_book=this_book,
+        this_book_review=this_book_review)
 
 
 @app.route("/add_review", methods=["GET", "POST"])
